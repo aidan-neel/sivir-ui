d163917f158f384096ea4b4930ed2d977421025a
aidan-neel <aaidan5899@gmail.com>
2025-04-22 17:46:41 -0500

    Final

---
+++ src/routes/docs/changelog/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<main class="flex flex-col w-full h-screen">
+	<header class="flex flex-col gap-1 pb-6 border-b rounded-none">
+		<p class="text-4xl font-bold">Changelog</p>
+		<p class="text-lg text-foreground-muted">A log of the latest updates</p>
+	</header>
+
+	<header class="flex flex-col gap-1 py-6 border-b">
+		<p class="text-2xl font-semibold">April 2025</p>
+	</header>
+
+	<div class="py-4">
+		<p class="text-xl font-semibold">v1.0.0 - Release</p>
+		<p class="text-base pt-2">
+			aidan-neel/ui has been released! Over the next couple months, I will be adding components,
+			polishing, and updating aidan-neel/ui as much as possible without getting burnt out.
+			<br /><br />
+			Check out some of the
+			<a href="/docs/components/alert" class="underline font-medium">components</a> and get started
+			with the
+			<a href="/docs/components/installation  " class="underline font-medium">installation</a> page.
+		</p>
+	</div>
+</main>
